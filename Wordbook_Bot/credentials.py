3708ef6922383932a4c7ef0a02108ab297b10ee9
Kyle Lobo <25124502+kylelobo@users.noreply.github.com>
2018-11-06 00:31:53 +0530

    Create credentials.py

---
+++ Wordbook_Bot/credentials.py
@@ -0,0 +1,11 @@
+# Reddit ID Pass
+reddit_username = ""
+reddit_password = ""
+
+# Reddit API ID & Key which can be found here: https://www.reddit.com/prefs/apps/
+client_id = ""
+client_secret = ""
+
+# Oxford Dictionary application ID & Key which can be found here: https://developer.oxforddictionaries.com/
+app_id = ""
+app_key = ""
