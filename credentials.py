c7da1e5b34350995073c9e94be2d5a94c67cea4c
Kyle Lobo <25124502+kylelobo@users.noreply.github.com>
2018-11-06 00:23:28 +0530

    Delete credentials.py

--- credentials.py
+++
@@ -1,6 +0,0 @@
-username = "" # Your Reddit username
-password = "" # Your Reddit password
-client_id = "" # Reddit User ID which can be found here: https://www.reddit.com/prefs/apps/
-client_secret = "" # Reddit Secret ID which can be found here: https://www.reddit.com/prefs/apps/
-app_id = "" # Oxfored Dictionary Application ID which can be found here: https://developer.oxforddictionaries.com
-app_key = "" # Oxfored Dictionary Application Key which can be found here: https://developer.oxforddictionaries.com
