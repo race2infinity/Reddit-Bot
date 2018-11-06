0ca1c80b49d05a19bc671227177158b5a1367e09
Kyle Lobo <25124502+kylelobo@users.noreply.github.com>
2018-11-07 03:11:49 +0530

    Removed credentials.py. The code now uses environment variables

--- Wordbook_Bot/bot_login.py
+++ Wordbook_Bot/bot_login.py
@@ -1,14 +1,13 @@
 import praw
 import os
-import credentials
 
 def bot_login():
     print ("Logging in..")
     try:
-        r = praw.Reddit(username = credentials.reddit_username,
-                password = credentials.reddit_password,
-                client_id = credentials.client_id,
-                client_secret = credentials.client_secret,
+        r = praw.Reddit(username = os.environ['reddit_username'],
+                password = os.environ['reddit_password'],
+                client_id = os.environ['client_id'],
+                client_secret = os.environ['client_secret'],
                 user_agent = "kyle")
         print ("Logged in!")
     except:
