ab0ae698680e6f954e9d0e3dcb9b953892098495
kylelobo <kylelobo11898@gmail.com>
2018-11-15 22:20:43 +0530

    Added delay when trying to re-connect

--- Wordbook_Bot/wordbook_bot.py
+++ Wordbook_Bot/wordbook_bot.py
@@ -5,13 +5,11 @@ import requests
 import json
 import bot_login
 
-
 def reply_to_comment(r, comment_id, comment_reply, dictionary_type, comment_subreddit, comment_author, comment_body):
     comment_to_be_replied_to = r.comment(id=comment_id)
     comment_to_be_replied_to.save()
     comment_to_be_replied_to.reply(comment_reply)
     print ("\nReply details:\nDictionary: {}\nSubreddit: r/{}\nComment: \"{}\"\nUser: u/{}\a". format(dictionary_type, comment_subreddit, comment_body, comment_author))
-
 
 def run_bot(r, after_utc):
     try:
@@ -132,7 +130,7 @@ def run_bot(r, after_utc):
 
                 # Prolly low karma so can't comment as frequently
                 except Exception as e:
-                    time_remaining = 5
+                    time_remaining = 15
                     if (str(e).split()[0] == "RATELIMIT:"):
                         for i in str(e).split():
                             if (i.isdigit()):
@@ -159,5 +157,8 @@ if __name__ == "__main__":
             print ("\nFetching comments..")
             while True:
                 after_utc = run_bot(r, after_utc)
+                time.sleep(2)
+
         except Exception as e:
             print (str(e.__class__.__name__) + ": " + str(e))
+            time.sleep(15)
