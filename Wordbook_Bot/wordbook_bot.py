b31612cb3293ab1f46f652d49838e39752b89659
kylelobo <kylelobo11898@gmail.com>
2018-11-16 22:31:15 +0530

    Increased delay for fetching comments, re-structured code

--- Wordbook_Bot/wordbook_bot.py
+++ Wordbook_Bot/wordbook_bot.py
@@ -5,159 +5,161 @@ import requests
 import json
 import bot_login
 
+def json_dump_and_parse(file_name, request):
+    with open(file_name, "w+") as f:
+        json.dump(request.json(), f, sort_keys = True, ensure_ascii = False, indent = 4)
+    with open(file_name) as f:
+        data = json.load(f)
+    return data
+
+def comment_save_check(r):
+    saved = r.user.me().saved(limit=None)
+    count = 1
+    for item in saved:
+        if count > 100:
+            comment = r.comment(id=item)
+            comment.unsave()
+        count += 1
+
 def reply_to_comment(r, comment_id, comment_reply, dictionary_type, comment_subreddit, comment_author, comment_body):
-    comment_to_be_replied_to = r.comment(id=comment_id)
-    comment_to_be_replied_to.save()
-    comment_to_be_replied_to.reply(comment_reply)
-    print ("\nReply details:\nDictionary: {}\nSubreddit: r/{}\nComment: \"{}\"\nUser: u/{}\a". format(dictionary_type, comment_subreddit, comment_body, comment_author))
-
-def run_bot(r, after_utc):
     try:
-        comment_url = "https://api.pushshift.io/reddit/search/comment/?q=!dict&sort=desc&size=25&fields=author,body,id,subreddit,created_utc&after=" + after_utc
-        comment_req = requests.get(comment_url)
-        with open("comment_data.json", "w+") as f:
-            json.dump(comment_req.json(), f, sort_keys = True, ensure_ascii = False, indent = 4)
+        comment_to_be_replied_to = r.comment(id=comment_id)
+        comment_to_be_replied_to.save()
+        comment_to_be_replied_to.reply(comment_reply)
+        print ("\nReply details:\nDictionary: {}\nSubreddit: r/{}\nComment: \"{}\"\nUser: u/{}\a". format(dictionary_type, comment_subreddit, comment_body, comment_author))
 
-        with open("comment_data.json") as f:
-            parsed_json = json.load(f)
+    # Probably low karma so can't comment as frequently
+    except Exception as e:
+        time_remaining = 15
+        if (str(e).split()[0] == "RATELIMIT:"):
+            for i in str(e).split():
+                if (i.isdigit()):
+                    time_remaining = int(i)
+                    break
+            if (not "seconds" or not "second" in str(e).split()):
+                time_remaining *= 60
 
-        if (len(parsed_json["data"]) > 0):
-            after_utc = parsed_json["data"][0]["created_utc"]
+        print (str(e.__class__.__name__) + ": " + str(e))
+        for i in range(time_remaining, 0, -5):
+            print ("Retrying in", i, "seconds..")
+            time.sleep(5)
 
-            for x in parsed_json["data"]:
-                try:
-                    comment_author = x["author"]
-                    comment_body = x["body"]
-                    comment_id = x["id"]
-                    comment_subreddit = x["subreddit"]
-                    comment_saved = r.comment(id=comment_id)
-                    if ("!dict" in comment_body.lower() and not comment_saved.saved and comment_author != "Wordbook_Bot"):
-                        print ("\n\nFound a comment!")
-                        comment_body_list = list(comment_body.split())[1:]
+def run_bot(r, created_utc):
+    try:
+        comment_url = "https://api.pushshift.io/reddit/search/comment/?q=!dict&sort=desc&size=50&fields=author,body,created_utc,id,subreddit&after=" + created_utc
 
-                        app_id = os.environ["app_id"]
-                        app_key = os.environ["app_key"]
-                        language = "en"
-                        word_id = " ".join(str(i) for i in comment_body_list)
+        parsed_comment_json = json_dump_and_parse("comment_data.json", requests.get(comment_url))
 
-                        url = "https://od-api.oxforddictionaries.com/api/v1/entries/" + language + "/" + word_id.replace(" ", "_").lower()
-                        req = requests.get(url, headers = {"app_id": app_id, "app_key": app_key})
+        if (len(parsed_comment_json["data"]) > 0):
+            created_utc = parsed_comment_json["data"][0]["created_utc"]
 
-                        # Oxford Dictionary
-                        if (req.status_code == 200):
-                            dictionary_type = "Oxford"
+            for comment in parsed_comment_json["data"]:
 
-                            with open("data.json", "w+") as f:
-                                json.dump(req.json(), f, sort_keys = True, ensure_ascii = False, indent = 4)
+                comment_author = comment["author"]
+                comment_body = comment["body"]
+                comment_id = comment["id"]
+                comment_subreddit = comment["subreddit"]
+                comment_saved = r.comment(id=comment_id)
+                
+                if ("!dict" in comment_body.lower() and not comment_saved.saved and comment_author != "Wordbook_Bot"):
+                    print ("\n\nFound a comment!")
+                    comment_body_list = list(comment_body.split())[1:]
 
-                            with open("data.json") as f:
-                                data = json.load(f)
+                    app_id = os.environ["app_id"]
+                    app_key = os.environ["app_key"]
+                    language = "en"
 
-                            comment_reply = "\n\n>" + word_id
+                    word = " ".join(str(i) for i in comment_body_list)
+
+                    oxford_url = "https://od-api.oxforddictionaries.com/api/v1/entries/" + language + "/" + word.replace(" ", "_").lower()
+                    request = requests.get(oxford_url, headers = {"app_id": app_id, "app_key": app_key})
+
+                    # Oxford Dictionary
+                    if (request.status_code == 200):
+                        dictionary_type = "Oxford"
+                        parsed_word_json = json_dump_and_parse("word_data.json", request)
+                        comment_reply = "\n\n>" + word
+
+                        try:
+                            lexicalCategory = parsed_word_json["results"][0]["lexicalEntries"][0]["lexicalCategory"].replace("[", "").replace("]", "")
+                            comment_reply += "\n\n\n\n**Part of speech:** \n\n*" + lexicalCategory + "*"
+                        except:
+                            lexicalCategory = ""
+
+                        try:
+                            definition = parsed_word_json["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0].replace("[", "").replace("]", "")
+                            comment_reply += "\n\n\n\n**Definition:** \n\n" + definition
+                        except:
+                            definition = ""
+
+                        try:
+                            example = parsed_word_json["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["examples"][0]["text"].replace("[", "").replace("]", "")
+                            comment_reply += "\n\n\n\n**Example:** \n\n" + example
+                        except:
+                            example = ""
+
+                        if (len(definition) > 0):
+                            source = "https://en.oxforddictionaries.com/definition/" + word.replace(" ", "_")
+                            comment_reply += "\n\n\n\n**Source:** " + source
+
+                    # Urban Dictionary
+                    else:
+                        urban_dict_url = "http://api.urbandictionary.com/v0/define?term={" + word + "}"
+                        request = requests.get(urban_dict_url)
+
+                        if (request.status_code == 200):
+                            dictionary_type = "Urban"
+                            parsed_word_json = json_dump_and_parse("word_data.json", requests.get(urban_dict_url))
+                            comment_reply = "\n\n>" + word
+
+                            if(len(parsed_word_json["list"]) == 0):
+                                comment_reply += "\n\nSorry, such a word does not exist!"
+                                dictionary_type = "None"
 
                             try:
-                                definition = data["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0].replace("[", "").replace("]", "")
+                                definition = parsed_word_json["list"][0]["definition"].replace("[", "").replace("]", "")
+                                comment_reply += "\n\n\n\n**Definition:** \n\n" + definition
                             except:
                                 definition = ""
 
                             try:
-                                lexicalCategory = data["results"][0]["lexicalEntries"][0]["lexicalCategory"].replace("[", "").replace("]", "")
-                            except:
-                                lexicalCategory = ""
-
-                            try:
-                                example = data["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["examples"][0]["text"].replace("[", "").replace("]", "")
+                                example = parsed_word_json["list"][0]["example"].replace("[", "").replace("]", "")
+                                comment_reply += "\n\n\n\n**Example:** \n\n" + example
                             except:
                                 example = ""
 
-                            if (len(lexicalCategory) > 0):
-                                comment_reply += "\n\n\n\n**Part of speech:** \n\n*" + lexicalCategory + "*"
                             if (len(definition) > 0):
-                                comment_reply += "\n\n\n\n**Definition:** \n\n" + definition
-                            if (len(example) > 0):
-                                comment_reply += "\n\n\n\n**Example:** \n\n" + example
-                            if (len(definition) > 0):
-                                source = "https://en.oxforddictionaries.com/definition/" + word_id.replace(" ", "_")
+                                source = "https://www.urbandictionary.com/define.php?term=" + word.replace(" ", "%20")
                                 comment_reply += "\n\n\n\n**Source:** " + source
 
-                        # Urban Dictionary
+                        # Word doesn't exist
                         else:
-                            url = "http://api.urbandictionary.com/v0/define?term={" + word_id + "}"
-                            req = requests.get(url)
-                            if (req.status_code == 200):
-                                dictionary_type = "Urban"
+                            comment_reply = "\n\nSorry, such a word does not exist!"
+                            dictionary_type = "None"
 
-                                with open("data.json", "w+") as f:
-                                    json.dump(req.json(), f, sort_keys = True, ensure_ascii = False, indent = 4)
+                    comment_reply += "\n\n\n\n---\n\n^(Beep boop. I am a bot. If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=PositivePlayer1&subject=/u/Wordbook_Bot)\n\n^(Want to make a similar reddit bot? Check out: ) [^GitHub ](https://github.com/kylelobo/Reddit-Bot)"
 
-                                with open("data.json") as f:
-                                    data = json.load(f)
+                    reply_to_comment(r, comment_id, comment_reply, dictionary_type, comment_subreddit, comment_author, comment_body)
 
-                                comment_reply = "\n\n>" + word_id
-
-                                if(len(data["list"]) == 0):
-                                    comment_reply += "\n\nSorry, such a word does not exist!"
-                                    dictionary_type = "None"
-
-                                try:
-                                    definition = data["list"][0]["definition"].replace("[", "").replace("]", "")
-                                except:
-                                    definition = ""
-
-                                try:
-                                    example = data["list"][0]["example"].replace("[", "").replace("]", "")
-                                except:
-                                    example = ""
-
-                                if (len(definition) > 0):
-                                    comment_reply += "\n\n\n\n**Definition:** \n\n" + definition
-                                if (len(example) > 0):
-                                    comment_reply += "\n\n\n\n**Example:** \n\n" + example
-                                if (len(definition) > 0):
-                                    source = "https://www.urbandictionary.com/define.php?term=" + word_id.replace(" ", "%20")
-                                    comment_reply += "\n\n\n\n**Source:** " + source
-
-                            # Word doesn't exist
-                            else:
-                                comment_reply = "\n\nSorry, such a word does not exist!"
-                                dictionary_type = "None"
-
-                        comment_reply += "\n\n\n\n---\n\n^(Beep boop. I am a bot. If there are any issues, contact my) [^Master ](https://www.reddit.com/message/compose/?to=PositivePlayer1&subject=/u/Wordbook_Bot)\n\n^(Want to make a similar reddit bot? Check out: ) [^GitHub ](https://github.com/kylelobo/Reddit-Bot)"
-
-                        reply_to_comment(r, comment_id, comment_reply, dictionary_type, comment_subreddit, comment_author, comment_body)
-
-                        print ("\nFetching comments..")
-
-                # Prolly low karma so can't comment as frequently
-                except Exception as e:
-                    time_remaining = 15
-                    if (str(e).split()[0] == "RATELIMIT:"):
-                        for i in str(e).split():
-                            if (i.isdigit()):
-                                time_remaining = int(i)
-                                break
-                        if (not "seconds" or not "second" in str(e).split()):
-                            time_remaining *= 60
-
-                    print (str(e.__class__.__name__) + ": " + str(e))
-                    for i in range(time_remaining, 0, -5):
-                        print ("Retrying in", i, "seconds..")
-                        time.sleep(5)
+                    print ("\nFetching comments..")
 
     except Exception as e:
         print (str(e.__class__.__name__) + ": " + str(e))
 
-    return str(after_utc)
+    return str(created_utc)
 
 if __name__ == "__main__":
     while True:
         try:
             r = bot_login.bot_login()
-            after_utc = ""
+            created_utc = ""
             print ("\nFetching comments..")
             while True:
-                after_utc = run_bot(r, after_utc)
-                time.sleep(2)
+                # Making sure only 100 comments are saved. This is because Reddit has a cap of 1000 saved comments
+                comment_save_check(r)
+                # Fetching all new comments that were created after created_utc time
+                created_utc = run_bot(r, created_utc)
+                time.sleep(5)
 
         except Exception as e:
             print (str(e.__class__.__name__) + ": " + str(e))
